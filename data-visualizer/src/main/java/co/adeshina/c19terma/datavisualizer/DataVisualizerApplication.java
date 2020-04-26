0bfc4026af08ef6e915e503d3262451638f7fcd2
Adeshina Ogunmodede <abdulwahabogunmodede@gmail.com>
2020-04-26 01:14:56 +0100

    initial commit

---
+++ data-visualizer/src/main/java/co/adeshina/c19terma/datavisualizer/DataVisualizerApplication.java
@@ -0,0 +1,13 @@
+package co.adeshina.c19terma.datavisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataVisualizerApplication.class, args);
+	}
+
+}
