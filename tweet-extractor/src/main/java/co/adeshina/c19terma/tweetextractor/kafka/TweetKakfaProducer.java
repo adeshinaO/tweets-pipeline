d374e1fe9273c9cdcfa64ca61e09ca7849483370
Adeshina Ogunmodede <abdulwahabogunmodede@gmail.com>
2020-05-01 00:53:08 +0100

    removed mongo dependency from POM

---
+++ tweet-extractor/src/main/java/co/adeshina/c19terma/tweetextractor/kafka/TweetKakfaProducer.java
@@ -0,0 +1,7 @@
+package co.adeshina.c19terma.tweetextractor.kafka;
+
+public class TweetKakfaProducer {
+
+    // todo: see kakfa docs and first demo for how to configure this.
+
+}
--- tweet-stream-processor/src/main/java/co/adeshina/c19terma/streamprocessor/TweetStreamProcessor.java
+++ tweet-stream-processor/src/main/java/co/adeshina/c19terma/streamprocessor/TweetStreamProcessor.java
@@ -4,9 +4,12 @@ package co.adeshina.c19terma.streamprocessor;
  * Hello world!
  *
  */
-public class TweetStreamProcessor
-{
+public class TweetStreamProcessor {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+
+
+
+
     }
 }
