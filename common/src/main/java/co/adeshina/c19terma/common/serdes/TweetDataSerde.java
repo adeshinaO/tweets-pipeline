dc8da6226b6d5789d2a95181851e38db69adbfb1
Ade <abdulwahabogunmodede@gmail.com>
2020-05-04 23:46:58 +0100

    Implement common module (#3)
    * Implemented DTO classes for Kafka data transport
    * Implement custom JSON serde using Jackson

---
+++ common/src/main/java/co/adeshina/c19terma/common/serdes/TweetDataSerde.java
@@ -0,0 +1,47 @@
+package co.adeshina.c19terma.common.serdes;
+
+import co.adeshina.c19terma.common.dto.TweetData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class TweetDataSerde implements Deserializer<TweetData>, Serializer<TweetData> {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public TweetData deserialize(String topic, byte[] data) {
+
+        if (data == null) {
+            return null;
+        }
+
+        try {
+            return objectMapper.readValue(new String(data), TweetData.class);
+        } catch (JsonProcessingException e) {
+            throw new SerdeException("Failed to deserialize", e);
+        }
+    }
+
+    @Override
+    public byte[] serialize(String topic, TweetData tweetData) {
+
+        if (tweetData == null) {
+            return null;
+        }
+
+        try {
+            return objectMapper.writeValueAsBytes(tweetData);
+        } catch (JsonProcessingException e) {
+            throw new SerdeException("Failed to serialize", e);
+        }
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) { }
+
+    @Override
+    public void close() { }
+}
--- tweet-stream-processor/src/main/java/co/adeshina/c19terma/streamprocessor/TweetStreamProcessor.java
+++ tweet-stream-processor/src/main/java/co/adeshina/c19terma/streamprocessor/TweetStreamProcessor.java
@@ -1,14 +1,7 @@
 package co.adeshina.c19terma.streamprocessor;
 
-/**
- * Hello world!
- *
- */
 public class TweetStreamProcessor {
     public static void main( String[] args ) {
-
-
-
 
 
     }
