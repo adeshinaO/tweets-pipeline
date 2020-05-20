79f37f869d3ab00d132dd91ecbd435c32ea91eb5
Adeshina Ogunmodede <abdulwahabogunmodede@gmail.com>
2020-05-20 04:21:44 +0100

    finished implementation of extractor program

---
+++ extractor/src/main/java/co/adeshina/c19terma/extractor/exception/ConfigPropertiesInitException.java
@@ -0,0 +1,8 @@
+package co.adeshina.c19terma.extractor.exception;
+
+public class ConfigPropertiesInitException extends Exception {
+
+    public ConfigPropertiesInitException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- tweet-extractor/src/main/java/co/adeshina/c19terma/tweetextractor/exception/TweetExtractorException.java
+++
@@ -1,9 +0,0 @@
-package co.adeshina.c19terma.tweetextractor.exception;
-
-// todo: is this in use??
-public class TweetExtractorException extends Exception {
-
-    public TweetExtractorException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
